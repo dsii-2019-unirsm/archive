8b2ecb5c5d415f6c4580de0e79d2681e5deda5bc
Grazianaf <47991263+grazianaf@users.noreply.github.com>
2019-05-21 10:09:11 +0200

    Update sketch.js

--- grazianaf/Variazioni10Print/10printp5/sketch.js
+++ grazianaf/Variazioni10Print/10printp5/sketch.js
@@ -1,57 +1,37 @@
-function setup(){
+var x = 0;
+var y = 0;
+//cella
+var move = 20;
+//probabilità fissa
+var prob = 0.5;
 
+function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(255);
-  stroke(0);
-  strokeWeight(3);
-  fill(255);
-  colorMode(HSB);
+  background (188,216,193);
 }
-
-var boxWidth = 50;
-var xPos = 0;
-var yPos = 0;
-
-function draw(){
-
-  //numero tra 0 e 1 a caso
-  var forma = random();
-  print(forma);
-  var h = random(255);
-  stroke(h, 30, 100);
-  fill(h, 30, 100);
-  
-  //se la probabilità è minore di x numero disegna forme (linee o triangoli in posizioni differenti
-  if(forma < 0.4){
-    line(xPos, yPos, xPos + boxWidth, yPos + boxWidth);
-  }else if(forma < 0.5){
-    triangle(xPos, yPos, xPos, yPos + boxWidth, xPos + boxWidth, yPos + boxWidth);
-  }else if(forma < 0.6){
-    triangle(xPos, yPos, xPos + boxWidth, yPos, xPos + boxWidth, yPos + boxWidth);
-  }else{
-    line(xPos, yPos + boxWidth, xPos + boxWidth, yPos);
+ function draw() {
+   stroke(50,83,61);
+   if (random(1) < prob) {
+     fill(50,83,61);
+     triangle(x, y, x, y + move, x + move, y + move);
+  } else {
+    triangle(x, y, x + move, y + move, x + move, y);
   }
-
-  // sposta la x della dimensione della cella per disegnare la cella successiva
-  xPos += boxWidth;
-  
-  //inizia una nuova riga
-  if(xPos > width){
-    xPos = 0;
-    yPos += boxWidth;
-  }
-
-  //se y è maggiore dell'altezza interrompe il loop
-  if(yPos > height){
-    noLoop();
-  }
-}
-
-function mousePressed(){
-  //resetta e riprende 
-  background(255);
-  xPos = 0;
-  yPos = 0;
-  loop();
-}
-
+   //disegna cella successiva
+   x = x + move;
+   //scorri riga
+   if (x > width){
+     x = 0;
+     y = y + move;
+   }
+   // se la y è maggiore dell'altezza sceglie un numero random per la dimensione della cella 
+   //tra 10 e 100 e rincomincia impostando la probabilità randomicamente 
+   if (y > (height - move)) {
+     background(0);
+     x = 0;
+     y = 0;
+     move = random(10, 100);
+     background(188,216,193);
+     prob = random(1);
+   }
+ }
