99b31aca0e1cc1cd327d126e54f4e2531fcc5e02
MrJ4ckpot <lmarchionni.drm@gmail.com>
2019-06-01 18:21:08 +0200

    Update sketch.js

--- Mrj4ckpot/00ProgettoFinale/Sketches/TaxelationRecognitionGetMap/sketch.js
+++ Mrj4ckpot/00ProgettoFinale/Sketches/TaxelationRecognitionGetMap/sketch.js
@@ -16,6 +16,9 @@ let prontoB=false;
 let classifier;
 
 let gino = false;
+let mappa = false;
+let daMappare;
+let imgsResolution;
 
 
 function carica(url, id){
@@ -28,7 +31,7 @@ let img = loadImage(url,  ready => { id=true;  });
 
 
 
-
+// precarico ml5 e le immagini sorgente
 function preload() {
   classifier = ml5.imageClassifier('MobileNet');
   A = carica("https://i.imgur.com/ONuv0Qc.jpg", prontoA);
@@ -37,34 +40,32 @@ function preload() {
 
 
 
-
-
-
+// ml5 riconosce l'immagine
 function gotResult(error, results) {
-  // Display error in the console
+
   if (error) {
     console.error(error);
   }
-  // The results are in an array ordered by confidence.
+
   console.log(results);
-  createDiv('Label: ' + results[0].label);
-  createDiv('Confidence: ' + nf(results[0].confidence, 0, 2));
+  daMappare = results[0].label;
 }
 
 
-
-
-
-
-
-
-
-
-
-function aCheAssimiglia(keywords) {
+// inserisce il suggerimento di ml5 e cerca l'immagine corrispettiva su unsplash
+function aCheAssimiglia(immagine, keywords) {
+  imgsResolution= immagine.width + "x" + immagine.height;
   var url = "https://source.unsplash.com/" + imgsResolution + "/?" + keywords + "&" + random(200); // < un random per caricarne sempre una nuova anche sugli stessi temi
   var img_Loading = loadImage(url);
   return img_Loading;
+}
+
+
+// inserisco gotResult e aChiAssomiglia dentro una funzione in modo da fermare il programma
+function indovina(){
+  gino = get();
+  classifier.classify(gino, gotResult);
+  mappa = aCheAssimiglia(gino, daMappare);
 }
 
 
@@ -76,8 +77,7 @@ function setup() {
 
     beholder = createImage(500, 500);
 
-  console.log("TOTALE "+(contaA+contaB)+"||||||||||||||||||"+contaA+" BLU"+"|||||||"+contaB+" ROSSO");
-
+  //console.log("TOTALE "+(contaA+contaB)+"||||||||||||||||||"+contaA+" BLU"+"|||||||"+contaB+" ROSSO");
 }
 
 function draw() {
@@ -111,12 +111,12 @@ background(30);
 
   }
 
+
+// DISPONE I TASSELLI DELLE DUE SORGENTI
   A.resize(beholder.width, beholder.height);
   B.resize(beholder.width, beholder.height);
 
-
     for (let x = 0; x < beholder.width-res; x+=res) {
-
       for (let y = 0; y < beholder.height-res; y+=res) {
 
         posx = x;
@@ -128,17 +128,32 @@ background(30);
            image(B, posx, posy, res, res, posx, posy, res, res);
            }
       }
-
-
     }
-if (gino) image(gino,0,0)
-fill(255);
-textSize(32);
-text('risultato', 500, 550);
+
+
+// STAMPA A SCHERMO L'IMMAGINE SORGENTE E LA BASE PER L'HEIGHTMAP
+if (gino){
+   image(gino,0,0);
+   mappa.resize(500,500);
+   image(mappa, 500, 0);
+
+
+fill(30);
+rect(0,500, 1000, 600);
+
+   fill(255);
+   textSize(32);
+   textAlign(CENTER);
+   text(daMappare, 500, 540);
+ }
+
 
 }
 
 
 function keyPressed(){
-  if (key == 'a') gino = get();
+ // TASTO A FA LO SCREEN DI BEHOLDER E TROVA UN'IMMAGINE CHE GLI SOMIGLIA
+  if (key == 'a') {
+      indovina();
+   }
 }
