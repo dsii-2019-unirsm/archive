20dcf358b981d17ab0857c0f15a35cf71ff8f505
MrJ4ckpot <lmarchionni.drm@gmail.com>
2019-06-02 00:19:46 +0200

    a

--- Mrj4ckpot/00ProgettoFinale/00Prototype/sketch.js
+++ Mrj4ckpot/00ProgettoFinale/00Prototype/sketch.js
@@ -1,3 +1,4 @@
+// VARIABILI DI BEHOLDER MAP RECOGNITION
 let A, B;
 let puntiA = [];
 let puntiB = [];
@@ -24,15 +25,29 @@ let posIniX;
 let posIniY;
 
 
+
+// VARIABILI DI BEHOLDER 3D EDITOR
+let easycam;
+let punti = [];
+let voxel = [];
+let voxelBN = [];
+
+let viewMode = 0;
+let rot=0;
+
+let multi = 1;
+
+let cam1;
+
+let colore;
+let u,v;
+
+
+
 function carica(url, id){
 let img = loadImage(url,  ready => { id=true;  });
   return img;
 }
-
-
-
-
-
 
 // precarico ml5 e le immagini sorgente
 function preload() {
@@ -48,6 +63,16 @@ function preload() {
 //  B.size(500, 500);
 //  B.hide();
 }
+
+
+
+//
+//
+//
+// FUNZIONI DI BEHOLDER MAP RECOGNITION
+//
+//
+//
 
 
 
@@ -85,6 +110,66 @@ function indovina(){
 }
 
 
+
+
+//
+//
+//
+// FUNZIONI DI BEHOLDER 3D EDITOR
+//
+//
+//
+
+function carica() {
+  // CARICA MAPPA DISPLACEMENT
+    mappa.loadPixels();
+    let f = 0;
+    let k = 0;
+    for (let x = 0; x < mappa.width-res; x+=res) {
+      punti[f] = [];
+      for (let y = 0; y < mappa.height-res; y+=res) {
+        let c = mappa.get(x, y);
+        let h = brightness(c);
+        punti[f][k] = h;
+        k++;
+      }
+      k=0;
+      f++;
+    }
+    mappa.updatePixels();
+
+  // CARICA TEXTURE
+    gino.loadPixels();
+    let i = 0;
+    let j = 0;
+    for (let x = 0; x < gino.width-res; x+=res) {
+      voxel[i] = [];
+      voxelBN[i] = [];
+      for (let y = 0; y < gino.height-res; y+=res) {
+        let c = gino.get(x, y);
+        let h = brightness(c);
+        voxel[i][j] = c;
+        voxelBN[i][j] = h;
+        j++;
+      }
+      j=0;
+      i++;
+    }
+    gino.updatePixels();
+}
+
+
+// // // // // //
+// // // //
+// //
+// SETUP
+// //
+// // // //
+// // // // // //
+
+
+
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
     background(220);
@@ -96,8 +181,46 @@ function setup() {
     posIniX=(windowWidth/2)-(beholder.width)/2-beholder.width;
     posIniY=(windowHeight/2)-beholder.width/2;
 
+
+// EASYCAM
+    Dw.EasyCam.prototype.apply = function(n) {
+        var o = this.cam;
+        n = n || o.renderer,
+        n && (this.camEYE = this.getPosition(this.camEYE), this.camLAT = this.getCenter(this.camLAT), this.camRUP = this.getUpVector(this.camRUP), n._curCamera.camera(this.camEYE[0], this.camEYE[1], this.camEYE[2], this.camLAT[0], this.camLAT[1], this.camLAT[2], this.camRUP[0], this.camRUP[1], this.camRUP[2]))
+
+      };
+
+    easycam = new Dw.EasyCam(_renderer, {
+      distance: 350,
+      center: [0,0,0]
+    });
+
+    easycam.setViewport([0, 0, windowWidth, windowHeight]);
+
+    // start with an animated rotation
+    easycam.setRotation(Dw.Rotation.create({
+      angles_xyz: [0, -PI / 3, PI / 2]
+    }), 4000);
+    easycam.setDistance(500, 2500);
+
   //console.log("TOTALE "+(contaA+contaB)+"||||||||||||||||||"+contaA+" BLU"+"|||||||"+contaB+" ROSSO");
 }
+
+
+
+
+
+// // // // // //
+// // // //
+// //
+// DRAW
+// //
+// // // //
+// // // // // //
+
+
+
+
 
 function draw() {
 
@@ -152,6 +275,7 @@ background(30);
     }
 //}
 
+// PREMI TASTO A:
 // STAMPA A SCHERMO L'IMMAGINE SORGENTE E LA BASE PER L'HEIGHTMAP
 if (gino && mappa){
    gino.resize(500,500);
@@ -160,18 +284,31 @@ if (gino && mappa){
   mappa.resize(500,500);
   image(mappa, (windowWidth/2)-250, (windowHeight/2)-250);
 
-fill(30);
-rect(0,0, windowWidth, posIniY);
+  fill(30);
+  rect(0,0, windowWidth, posIniY);
 
   fill(255);
   textSize(32);
   textAlign(CENTER);
+
   text("It looks like a "+daMappare, windowWidth/2, (windowHeight/2)-300);
+
+
+  // PREMI TASTO S:
+  // CREA IL MODELLO 3D CON TEXTURE
 
 
  }
 
 
+noFill();
+strokeWeight(3);
+stroke(240);
+
+rect(posIniX, posIniY, beholder.width, beholder.height);
+rect(posIniX+beholder.width, posIniY, beholder.width, beholder.height);
+rect(posIniX+2*beholder.width, posIniY, beholder.width, beholder.height);
+strokeWeight(0);
 }
 
 
